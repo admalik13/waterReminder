791b15a91eebf8d31036a2bc3e94dbc5ff7236d5
Aditya Malik <ADMALIK@amdocs.com>
2018-05-28 11:27:48 +0530

    Initial commit

---
+++ drink/src/main/java/com/gedify/waterreminder/drink/InstantActivity.java
@@ -0,0 +1,13 @@
+package com.gedify.waterreminder.drink;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instant);
+    }
+}
